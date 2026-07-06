1573b99c100bd1e58c7f334b2e2916785bbfcc77
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-07 00:29:24 +0100

    Create education_stage_model.dart

---
+++ lib/models/education_stage_model.dart
@@ -0,0 +1,11 @@
+class EducationStageModel {
+  final String stageId;
+  final String title;
+  final String description;
+
+  EducationStageModel({
+    required this.stageId,
+    required this.title,
+    required this.description,
+  });
+}
