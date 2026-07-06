2dd19c4f06a499e4270608ad8d82d93f50c01469
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-07 00:30:19 +0100

    Create education_stage_data.dart

---
+++ lib/data/education_stage_data.dart
@@ -0,0 +1,33 @@
+import '../models/education_stage_model.dart';
+
+final List<EducationStageModel> educationStages = [
+
+  EducationStageModel(
+    stageId: "AFTER_10",
+    title: "After Class 10",
+    description:
+        "Explore streams, courses and career options after Class 10",
+  ),
+
+  EducationStageModel(
+    stageId: "AFTER_12",
+    title: "After Class 12",
+    description:
+        "Choose higher education and career pathways",
+  ),
+
+  EducationStageModel(
+    stageId: "UG",
+    title: "Undergraduate",
+    description:
+        "Explore degree programs and professional careers",
+  ),
+
+  EducationStageModel(
+    stageId: "PG",
+    title: "Postgraduate",
+    description:
+        "Advanced study and specialization options",
+  ),
+
+];
