5a71c35976c6396dda6de9c1fb3fd8fa1081a8fc
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-07 00:33:16 +0100

    Create stream_model.dart

---
+++ lib/models/stream_model.dart
@@ -0,0 +1,13 @@
+class StreamModel {
+  final String streamId;
+  final String title;
+  final String stageId;
+  final String description;
+
+  StreamModel({
+    required this.streamId,
+    required this.title,
+    required this.stageId,
+    required this.description,
+  });
+}
