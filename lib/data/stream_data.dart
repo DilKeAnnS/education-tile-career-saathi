913be9b7e1caee2864ab62853a64462dc324e41b
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-07 00:34:12 +0100

    Create stream_data.dart

---
+++ lib/data/stream_data.dart
@@ -0,0 +1,37 @@
+import '../models/stream_model.dart';
+
+final List<StreamModel> streams = [
+
+  StreamModel(
+    streamId: "SCIENCE",
+    title: "Science",
+    stageId: "AFTER_10",
+    description:
+        "Explore Medical, Engineering, Research and Technology careers",
+  ),
+
+  StreamModel(
+    streamId: "COMMERCE",
+    title: "Commerce",
+    stageId: "AFTER_10",
+    description:
+        "Explore Finance, Business and Management careers",
+  ),
+
+  StreamModel(
+    streamId: "ARTS",
+    title: "Arts",
+    stageId: "AFTER_10",
+    description:
+        "Explore Law, Government, Design and Social careers",
+  ),
+
+  StreamModel(
+    streamId: "SKILL",
+    title: "Skill Education",
+    stageId: "AFTER_10",
+    description:
+        "Explore vocational and skill based careers",
+  ),
+
+];
