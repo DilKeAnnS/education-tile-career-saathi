3794ef7f4b8c3fa61b6b8732de2ee17a01d2a98c
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-07 00:21:45 +0100

    Create career_model.dart

---
+++ lib/models/career_model.dart
@@ -0,0 +1,26 @@
+class CareerModel {
+  final String careerId;
+  final String title;
+  final String category;
+  final String overview;
+
+  final List<String> roadmap;
+  final List<String> courses;
+  final List<String> exams;
+  final List<String> skills;
+  final List<String> colleges;
+  final List<String> scholarships;
+
+  CareerModel({
+    required this.careerId,
+    required this.title,
+    required this.category,
+    required this.overview,
+    required this.roadmap,
+    required this.courses,
+    required this.exams,
+    required this.skills,
+    required this.colleges,
+    required this.scholarships,
+  });
+}
