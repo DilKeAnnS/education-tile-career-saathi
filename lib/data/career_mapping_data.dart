d190c8755171004f070804dafb01792283c2e528
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-07 00:41:21 +0100

    Update career_mapping_data.dart

--- lib/data/career_mapping_data.dart
+++ lib/data/career_mapping_data.dart
@@ -1,1 +1,21 @@
+class CareerMapping {
+  final String streamId;
+  final List<String> careerIds;
 
+  CareerMapping({
+    required this.streamId,
+    required this.careerIds,
+  });
+}
+
+
+final List<CareerMapping> careerMappings = [
+
+  CareerMapping(
+    streamId: "SCIENCE",
+    careerIds: [
+      "MED_DOCTOR_001",
+    ],
+  ),
+
+];
