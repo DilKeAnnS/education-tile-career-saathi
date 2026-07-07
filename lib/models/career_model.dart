62acb680ce6ada59d7e87dfe671544554ce4f3df
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-07 01:22:41 +0100

    Update career_model.dart

--- lib/models/career_model.dart
+++ lib/models/career_model.dart
@@ -4,6 +4,9 @@ class CareerModel {
   final String category;
   final String overview;
 
+  final List<String> suitableStages;
+  final List<String> relatedStreams;
+
   final List<String> roadmap;
   final List<String> courses;
   final List<String> exams;
@@ -11,16 +14,26 @@ class CareerModel {
   final List<String> colleges;
   final List<String> scholarships;
 
+  final String duration;
+  final String futureScope;
+
   CareerModel({
     required this.careerId,
     required this.title,
     required this.category,
     required this.overview,
+
+    required this.suitableStages,
+    required this.relatedStreams,
+
     required this.roadmap,
     required this.courses,
     required this.exams,
     required this.skills,
     required this.colleges,
     required this.scholarships,
+
+    required this.duration,
+    required this.futureScope,
   });
 }
