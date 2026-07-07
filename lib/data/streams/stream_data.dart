3b5873fccac5d68ea58f2086935cad9af2a8b52b
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-07 19:21:01 +0100

    Update stream_data.dart

--- lib/data/streams/stream_data.dart
+++ lib/data/streams/stream_data.dart
@@ -1,2 +1,132 @@
-// CareerSathi Stream Master Data
-// Data will be migrated from old stream_data.dart
+import '../../models/stream_model.dart';
+
+final List<StreamModel> streams = [
+
+  StreamModel(
+    streamId: "MEDICAL",
+    title: "Medical & Healthcare",
+    description:
+        "Explore medicine, healthcare, pharmacy and life science careers",
+    applicableStages: [
+      "AFTER_10",
+      "AFTER_12",
+      "UG",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "NON_MEDICAL",
+    title: "Non Medical & Technology",
+    description:
+        "Explore engineering, technology, AI and research careers",
+    applicableStages: [
+      "AFTER_10",
+      "AFTER_12",
+      "UG",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "COMMERCE",
+    title: "Commerce & Finance",
+    description:
+        "Explore finance, accounting, business and management careers",
+    applicableStages: [
+      "AFTER_10",
+      "AFTER_12",
+      "UG",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "ARTS",
+    title: "Arts & Humanities",
+    description:
+        "Explore law, government, social science and creative careers",
+    applicableStages: [
+      "AFTER_10",
+      "AFTER_12",
+      "UG",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "SPORTS",
+    title: "Sports",
+    description:
+        "Explore sports, coaching and professional sports careers",
+    applicableStages: [
+      "AFTER_10",
+      "AFTER_12",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "AGRICULTURE",
+    title: "Agriculture",
+    description:
+        "Explore agriculture, farming technology and allied careers",
+    applicableStages: [
+      "AFTER_10",
+      "AFTER_12",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "DESIGN",
+    title: "Design",
+    description:
+        "Explore fashion, UI/UX, interior and creative design careers",
+    applicableStages: [
+      "AFTER_10",
+      "AFTER_12",
+      "UG",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "ARTIST",
+    title: "Artist & Creative Field",
+    description:
+        "Explore art, media and creative professional careers",
+    applicableStages: [
+      "AFTER_10",
+      "AFTER_12",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "SKILLED",
+    title: "Vocational & Skill Courses",
+    description:
+        "Explore ITI, vocational and employment-focused skills",
+    applicableStages: [
+      "AFTER_10",
+      "ITI_VOCATIONAL",
+      "SKILL",
+    ],
+  ),
+
+
+  StreamModel(
+    streamId: "ABROAD",
+    title: "Foreign Education",
+    description:
+        "Explore study abroad and international education pathways",
+    applicableStages: [
+      "AFTER_12",
+      "UG",
+      "PG",
+      "FOREIGN",
+    ],
+  ),
+
+];
