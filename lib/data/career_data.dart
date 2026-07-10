0cd303b74c620e095d2913e0718ceaaffd7f9a17
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-10 22:48:52 +0100

    Phase 2 - Refactor Career Master Data Integration v1.1
    Separated career modules from master career data.
    
    Updated:
    - career_data.dart as central aggregator
    - Medical careers module connection
    
    Removed duplicate career storage.

--- lib/data/career_data.dart
+++ lib/data/career_data.dart
@@ -1,67 +1,10 @@
-class CareerModel {
-  final String careerId;
+import '../models/career_model.dart';
 
-  final String title;
-  final String category;
-  final String careerType;
+import 'careers/medical_careers.dart';
 
-  final String overview;
 
-  final List<String> suitableStages;
-  final List<String> relatedStreams;
+final List<CareerModel> careerData = [
 
-  final List<String> roadmapIds;
-  final List<String> courseIds;
-  final List<String> examIds;
-  final List<String> collegeIds;
-  final List<String> scholarshipIds;
+  ...medicalCareers,
 
-  final List<String> skills;
-  final List<String> jobRoles;
-  final List<String> keywords;
-
-  final String duration;
-  final String futureScope;
-  final String averageSalary;
-
-  final String officialSource;
-  final String officialWebsite;
-
-  final String academicYear;
-  final String lastUpdated;
-  final String verificationStatus;
-
-  CareerModel({
-    required this.careerId,
-
-    required this.title,
-    required this.category,
-    required this.careerType,
-
-    required this.overview,
-
-    required this.suitableStages,
-    required this.relatedStreams,
-
-    required this.roadmapIds,
-    required this.courseIds,
-    required this.examIds,
-    required this.collegeIds,
-    required this.scholarshipIds,
-
-    required this.skills,
-    required this.jobRoles,
-    required this.keywords,
-
-    required this.duration,
-    required this.futureScope,
-    required this.averageSalary,
-
-    required this.officialSource,
-    required this.officialWebsite,
-
-    required this.academicYear,
-    required this.lastUpdated,
-    required this.verificationStatus,
-  });
-}
+];
