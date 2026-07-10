94d556f8598e5386620e2a3610a4f5356e156e3b
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-10 22:21:22 +0100

    Phase 2 - Add Medical College Data Foundation v1.0
    Added college database foundation.
    
    Created:
    - AIIMS New Delhi profile
    - Government Medical College category
    - Career and course mapping
    - Verification fields

--- lib/data/colleges/college_data.dart
+++ lib/data/colleges/college_data.dart
@@ -1,2 +1,131 @@
-// CareerSathi Master Data
-// Data will be added from Excel
+import '../../models/college_model.dart';
+
+final List<CollegeModel> colleges = [
+
+  CollegeModel(
+    collegeId: "COLLEGE_AIIMS",
+
+    name: "AIIMS New Delhi",
+
+    category: "Medical",
+
+    description:
+        "All India Institute of Medical Sciences New Delhi is a premier medical institute of India.",
+
+
+    state: "Delhi",
+
+    city: "New Delhi",
+
+
+    relatedStreams: [
+      "MEDICAL",
+    ],
+
+
+    relatedCourses: [
+      "COURSE_MBBS",
+      "COURSE_MD",
+      "COURSE_MS",
+    ],
+
+
+    relatedCareers: [
+      "MED_DOCTOR_001",
+    ],
+
+
+    collegeType:
+        "Government Medical Institute",
+
+
+    admissionProcess:
+        "Admission through NEET UG and NEET PG examinations",
+
+
+    officialWebsite:
+        "https://www.aiims.edu",
+
+
+    rankingInfo:
+        "One of India's leading medical institutes",
+
+
+    academicYear:
+        "2026",
+
+
+    lastUpdated:
+        "2026-07",
+
+
+    verificationStatus:
+        "Verified",
+  ),
+
+
+
+  CollegeModel(
+    collegeId: "COLLEGE_GOVT_MEDICAL",
+
+    name: "Government Medical Colleges",
+
+    category: "Medical",
+
+    description:
+        "Government medical colleges provide MBBS and postgraduate medical education across India.",
+
+
+    state:
+        "Multiple States",
+
+    city:
+        "Multiple Cities",
+
+
+    relatedStreams: [
+      "MEDICAL",
+    ],
+
+
+    relatedCourses: [
+      "COURSE_MBBS",
+      "COURSE_MD",
+      "COURSE_MS",
+    ],
+
+
+    relatedCareers: [
+      "MED_DOCTOR_001",
+    ],
+
+
+    collegeType:
+        "Government Medical College",
+
+
+    admissionProcess:
+        "Admission through NEET UG and NEET PG counselling",
+
+
+    officialWebsite:
+        "https://www.nmc.org.in",
+
+
+    rankingInfo:
+        "Recognized medical education institutions approved by authorities",
+
+
+    academicYear:
+        "2026",
+
+
+    lastUpdated:
+        "2026-07",
+
+
+    verificationStatus:
+        "Verified",
+  ),
+
+];
