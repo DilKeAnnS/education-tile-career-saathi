4589335b997f7897e13716d61875c9119444f758
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-10 22:16:14 +0100

    Phase 2 - Add Medical Exam Data Foundation v1.0
    Added exam database foundation.
    
    Created:
    - NEET UG
    - NEET PG
    
    Linked exams with:
    - Medical stream
    - Doctor career
    - Medical courses

--- lib/data/exams/exam_data.dart
+++ lib/data/exams/exam_data.dart
@@ -1,2 +1,137 @@
-// CareerSathi Master Data
-// Data will be added from Excel
+import '../../models/exam_model.dart';
+
+final List<ExamModel> exams = [
+
+  ExamModel(
+    examId: "EXAM_NEET_UG",
+
+    title: "NEET UG",
+
+    category: "Medical Entrance",
+
+    description:
+        "National Eligibility cum Entrance Test for admission to undergraduate medical courses.",
+
+
+    applicableStages: [
+      "AFTER_12",
+    ],
+
+
+    relatedStreams: [
+      "MEDICAL",
+    ],
+
+
+    relatedCourses: [
+      "COURSE_MBBS",
+      "COURSE_BDS",
+      "COURSE_BAMS",
+      "COURSE_BHMS",
+    ],
+
+
+    relatedCareers: [
+      "MED_DOCTOR_001",
+    ],
+
+
+    conductingBody:
+        "National Testing Agency (NTA)",
+
+
+    examType:
+        "UNDERGRADUATE ENTRANCE EXAM",
+
+
+    eligibility:
+        "Class 12 with Physics, Chemistry and Biology/Biotechnology with required criteria",
+
+
+    frequency:
+        "Yearly",
+
+
+    officialWebsite:
+        "https://neet.nta.nic.in",
+
+
+    academicYear:
+        "2026",
+
+
+    lastUpdated:
+        "2026-07",
+
+
+    verificationStatus:
+        "Verified",
+  ),
+
+
+
+  ExamModel(
+    examId: "EXAM_NEET_PG",
+
+    title: "NEET PG",
+
+    category: "Medical Entrance",
+
+    description:
+        "National level entrance examination for postgraduate medical courses.",
+
+
+    applicableStages: [
+      "PG",
+    ],
+
+
+    relatedStreams: [
+      "MEDICAL",
+    ],
+
+
+    relatedCourses: [
+      "COURSE_MD",
+      "COURSE_MS",
+    ],
+
+
+    relatedCareers: [
+      "MED_DOCTOR_001",
+    ],
+
+
+    conductingBody:
+        "National Board of Examinations (NBE)",
+
+
+    examType:
+        "POSTGRADUATE ENTRANCE EXAM",
+
+
+    eligibility:
+        "MBBS degree with required registration criteria",
+
+
+    frequency:
+        "Yearly",
+
+
+    officialWebsite:
+        "https://natboard.edu.in",
+
+
+    academicYear:
+        "2026",
+
+
+    lastUpdated:
+        "2026-07",
+
+
+    verificationStatus:
+        "Verified",
+  ),
+
+];
