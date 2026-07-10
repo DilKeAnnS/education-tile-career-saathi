670a49505340cdbefa73417dcb4702b41ad84edf
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-10 20:50:49 +0100

    Milestone 2.1 - EducationStageModel v1.0

--- lib/models/education_stage_model.dart
+++ lib/models/education_stage_model.dart
@@ -3,9 +3,17 @@ class EducationStageModel {
   final String title;
   final String description;
 
+  // Display order in the app
+  final int displayOrder;
+
+  // Active / Inactive status
+  final bool isActive;
+
   EducationStageModel({
     required this.stageId,
     required this.title,
     required this.description,
+    required this.displayOrder,
+    required this.isActive,
   });
 }
