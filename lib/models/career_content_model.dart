5396654edf50deab273b26e34a2ade0c90575e41
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-11 21:27:29 +0100

    Phase 3 - Create Career Content Model v1.0

--- lib/models/career_content_model.dart
+++ lib/models/career_content_model.dart
@@ -1,1 +1,83 @@
+class CareerContentModel {
+  final String careerId;
 
+  // Basic Information
+  final String titleHindi;
+  final String titleEnglish;
+
+  // Introduction
+  final String overviewHindi;
+  final String overviewEnglish;
+
+  // Career Details
+  final List<String> responsibilities;
+  final List<String> workEnvironment;
+
+  // Eligibility
+  final List<String> eligibility;
+  final List<String> requiredSkills;
+
+  // Career Growth
+  final List<String> careerOpportunities;
+  final List<String> governmentOpportunities;
+  final List<String> privateOpportunities;
+  final List<String> selfEmploymentOptions;
+  final List<String> higherStudyOptions;
+
+  // Salary
+  final String averageSalary;
+  final String salaryGrowth;
+
+  // Pros & Challenges
+  final List<String> advantages;
+  final List<String> challenges;
+
+  // Future
+  final String futureScope;
+
+  // Official
+  final String officialSource;
+  final String officialWebsite;
+
+  // Verification
+  final String academicYear;
+  final String lastUpdated;
+  final String verificationStatus;
+
+  CareerContentModel({
+    required this.careerId,
+
+    required this.titleHindi,
+    required this.titleEnglish,
+
+    required this.overviewHindi,
+    required this.overviewEnglish,
+
+    required this.responsibilities,
+    required this.workEnvironment,
+
+    required this.eligibility,
+    required this.requiredSkills,
+
+    required this.careerOpportunities,
+    required this.governmentOpportunities,
+    required this.privateOpportunities,
+    required this.selfEmploymentOptions,
+    required this.higherStudyOptions,
+
+    required this.averageSalary,
+    required this.salaryGrowth,
+
+    required this.advantages,
+    required this.challenges,
+
+    required this.futureScope,
+
+    required this.officialSource,
+    required this.officialWebsite,
+
+    required this.academicYear,
+    required this.lastUpdated,
+    required this.verificationStatus,
+  });
+}
