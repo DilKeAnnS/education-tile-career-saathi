1cb0c7b961d49b7e12b724fcf57c0804ae4b2121
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-16 22:33:56 +0100

    Update mbbs_colleges_master.dart
    Import all state/UT college files
    Create one master list
    Provide single access point for:
    Search
    Filters
    AI
    Firebase migration
    Flutter UI

--- lib/data/medical/mbbs/colleges/mbbs_colleges_master.dart
+++ lib/data/medical/mbbs/colleges/mbbs_colleges_master.dart
@@ -1,1 +1,13 @@
+import 'andhra_pradesh_mbbs_colleges.dart';
+import 'haryana_mbbs_colleges.dart';
+import 'delhi_mbbs_colleges.dart';
+// remaining imports
 
+
+final List<CollegeModel> allMbbsColleges = [
+
+  ...andhraPradeshMbbsColleges,
+  ...haryanaMbbsColleges,
+  ...delhiMbbsColleges,
+
+];
