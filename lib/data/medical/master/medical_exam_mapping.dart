95b0af764a0b35c1974e298a76daa86d76bd7c96
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-07-23 18:03:26 +0100

    medical: initialize medical exam mapping

--- lib/data/medical/master/medical_exam_mapping.dart
+++ lib/data/medical/master/medical_exam_mapping.dart
@@ -1,1 +1,19 @@
+/// =============================================================
+/// Career Saathi
+/// Medical Exam Mapping
+///
+/// Version : 1.0.0
+/// Status  : Draft
+/// =============================================================
 
+class MedicalExamMapping {
+
+  static const List<String> mbbs = [
+    "EXAM_NEET_UG",
+  ];
+
+  static const List<String> bds = [
+    "EXAM_NEET_UG",
+  ];
+
+}
